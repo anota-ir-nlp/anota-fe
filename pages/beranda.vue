fa3ae458a251784f43cd3a026faa0f1f4431e9b1
Alden Luthfi <alden.arrahman@tiket.com>
2025-10-17 00:17:30 +0700

    small tweak

--- pages/beranda.vue
+++ pages/beranda.vue
@@ -96,11 +96,6 @@
               </p>
             </div>
           </div>
-          <div class="mt-4 p-3 bg-blue-50 border border-blue-200 rounded-lg">
-            <p class="text-sm text-blue-800">
-              <strong>Note:</strong> As an Admin, your project assignment is managed by system administrators.
-            </p>
-          </div>
         </Card>
 
         <!-- Kepala Riset Project Selection -->
